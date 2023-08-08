09a1a1c07b154bdba9cde8ea75d18026bd7c2b35
iQube <prakasam2021@gmail.com>
2023-08-08 19:54:41 +0530

    ui completed--finally!!!!!!

--- main.js
+++ main.js
@@ -5,35 +5,34 @@ const codetag = document.getElementById("codetag");
 const page1 = document.getElementById("page1");
 const getCode = document.getElementById("editing");
 const codeTag = document.getElementById("highlighting");
-const convert = document.getElementById("JSONtohtml");
+const convertJSON = document.getElementById("convert");
+const previewArea = document.getElementById("previewArea");
 
-// var m_pos;
-// function resizeRight(e) {
-//   var rightParent = resizeElementright.parentNode;
-//   var dx = m_pos - e.x;
-//   m_pos = e.x;
-//   rightParent.style.width =
-//     parseInt(getComputedStyle(rightParent, "").width) + dx + "px";
-// }
+var m_pos;
+function resizeRight(e) {
+  var rightParent = resizeElementright.parentNode;
+  var dx = m_pos - e.x;
+  m_pos = e.x;
+  rightParent.style.width =
+    parseInt(getComputedStyle(rightParent, "").width) + dx + "px";
+}
 
-// var resizeElementright = document.getElementById("preview-button");
-// resizeElementright.addEventListener(
-//   "mousedown",
-//   function (e) {
-//     m_pos = e.x;
-//     document.addEventListener("mousemove", resizeRight, false);
-//   },
-//   false
-// );
-// document.addEventListener(
-//   "mouseup",
-//   function () {
-//     document.removeEventListener("mousemove", resizeRight, false);
-//   },
-//   false
-// );
-
-//
+var resizeElementright = document.getElementById("preview-button");
+resizeElementright.addEventListener(
+  "mousedown",
+  function (e) {
+    m_pos = e.x;
+    document.addEventListener("mousemove", resizeRight, false);
+  },
+  false
+);
+document.addEventListener(
+  "mouseup",
+  function () {
+    document.removeEventListener("mousemove", resizeRight, false);
+  },
+  false
+);
 
 //setting up the clicked Effect
 function barClicked() {
@@ -50,15 +49,21 @@ function htmlEntities(str) {
 }
 
 getCode.oninput = () => {
-  // if (getCode.value[getCode.value.length - 1] == "\n") {
-  //   getCode.value += " ";
-  // }
   const html = Prism.highlight(
     getCode.value,
     Prism.languages.javascript,
     "javascript"
   );
+
   console.log(getCode.value);
   console.log(html);
   codeTag.innerHTML = html;
 };
+
+convertJSON.addEventListener("click", () => {
+  const JSONobj = JSON.parse(getCode.value);
+  const forms = [...JSONobj];
+  console.log(forms);
+
+  forms.forEach((page) => {});
+});
